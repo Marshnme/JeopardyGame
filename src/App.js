cb9205a1a9d36d5a3e4e242b71771dda8dc28cb2
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-11-15 00:51:08 -0500

    cors error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Route} from "react-router-dom"
 import Tribute from "./components/Tribute";
+import Game from "./components/Game";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Route path="/" component = {Tribute}/>
+      <Route path="/"  component = {Game}/>
+      <Route path="/tribute"  component = {Tribute}/>
+      
     </div>
   );
 }
